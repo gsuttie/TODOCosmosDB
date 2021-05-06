c1cc5e0b0b9ba83f47d16ccf227157adfa09a995
gregor suttie <gsuttie2001@hotmail.com>
2021-05-06 19:21:46 +0100

    Add project files.

---
+++ Services/ICosmosDbService.cs
@@ -0,0 +1,15 @@
+﻿namespace TODOCosmosDB.Services
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using TODOCosmosDB.Models;
+
+    public interface ICosmosDbService
+    {
+        Task<IEnumerable<Item>> GetItemsAsync(string query);
+        Task<Item> GetItemAsync(string id);
+        Task AddItemAsync(Item item);
+        Task UpdateItemAsync(string id, Item item);
+        Task DeleteItemAsync(string id);
+    }
+}
